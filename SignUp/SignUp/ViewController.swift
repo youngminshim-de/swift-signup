17c26e4a19410dc1149b683515256cc62e5bfdf7
shimyoungmin <dudals6965@gmail.com>
2021-03-29 16:31:58 +0900

    keyboard return 버튼 누르면 키보드 내리는 함수 구현

--- SignUp/SignUp/ViewController.swift
+++ SignUp/SignUp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var id: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var recheckPassword: UITextField!
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var idCheckLabel: UILabel!
+    @IBOutlet weak var passwordCheckLabel: UILabel!
+    @IBOutlet weak var passwordReCheckLabel: UILabel!
+    @IBOutlet weak var userNameCheckLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        id.addTarget(self, action: #selector(didEndExit(_:)), for: UIControl.Event.editingDidEndOnExit)
+        password.addTarget(self, action: #selector(didEndExit(_:)), for: UIControl.Event.editingDidEndOnExit)
+        recheckPassword.addTarget(self, action: #selector(didEndExit(_:)), for: UIControl.Event.editingDidEndOnExit)
+        userName.addTarget(self, action: #selector(didEndExit(_:)), for: UIControl.Event.editingDidEndOnExit)
     }
-
-
+    @objc func didEndExit(_ sender: UITextField) {
+        if password.isFirstResponder {
+            userName.becomeFirstResponder()
+        }
+    }
 }
 
